29b8ccfa39c8b80032387fab39cef372345be26e
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2020-10-31 17:30:56 +0100

    Handle further other images and add Fedora generation

--- gitlab-ci/libvirt/base.sh
+++ gitlab-ci/libvirt/base.sh
@@ -5,7 +5,16 @@
 # /opt/libivrt-driver/base.sh
 
 VM_IMAGES_PATH="/var/lib/libvirt/images"
+if [ "${CUSTOM_ENV_USE_FEDORA_IMAGE}" = 1 ]; then
+BASE_VM_IMAGE="$VM_IMAGES_PATH/gitlab-runner-fedora.qcow2"
+elif [ "${CUSTOM_ENV_USE_CENTOS_IMAGE}" = 1 ]; then
+BASE_VM_IMAGE="$VM_IMAGES_PATH/gitlab-runner-centos.qcow2"
+elif [ "${CUSTOM_ENV_USE_UBUNTU_IMAGE}" = 1 ]; then
+BASE_VM_IMAGE="$VM_IMAGES_PATH/gitlab-runner-ubuntu.qcow2"
+else
+# Base image is Debian 10+
 BASE_VM_IMAGE="$VM_IMAGES_PATH/gitlab-runner-base.qcow2"
+fi
 VM_ID="runner-$CUSTOM_ENV_CI_RUNNER_ID-project-$CUSTOM_ENV_CI_PROJECT_ID-concurrent-$CUSTOM_ENV_CI_CONCURRENT_PROJECT_ID-job-$CUSTOM_ENV_CI_JOB_ID"
 VM_IMAGE="$VM_IMAGES_PATH/$VM_ID.qcow2"
 
--- gitlab-ci/libvirt/generate-debian.sh
+++ gitlab-ci/libvirt/generate-debian.sh
@@ -18,6 +18,10 @@ virt-builder debian-10 \
     --install gitlab-runner,git,git-lfs,openssh-server \
     --run-command "git lfs install --skip-repo" \
     --ssh-inject gitlab-runner:file:/root/.ssh/id_rsa.pub \
+    --run-command "rm -f /root/.ssh/know_hosts" \
+    --upload /home/user/ci-keys/id_rsa:/home/gitlab-runner/.ssh/id_rsa \
+    --run-command "chown gitlab-runner:gitlab-runner /home/gitlab-runner/.ssh/id_rsa" \
+    --run-command "chmod 0600 /home/gitlab-runner/.ssh/id_rsa" \
     --run-command "echo 'gitlab-runner ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers" \
     --run-command "sed -E 's/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"net.ifnames=0 biosdevname=0\"/' -i /etc/default/grub" \
     --run-command "grub-mkconfig -o /boot/grub/grub.cfg" \
