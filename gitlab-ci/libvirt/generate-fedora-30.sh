1d374e6cb04a9a9710020bd6bcc69489c6fdc7ca
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2020-11-22 14:39:10 +0100

    Update basevm image generation using gitlab-runner binary

--- gitlab-ci/libvirt/generate-fedora-30.sh
+++ gitlab-ci/libvirt/generate-fedora-30.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+generate-fedora.sh#!/bin/bash
 
 set -x
 
-virt-builder fedora-33 \
+virt-builder fedora-30 \
     --smp 4 \
     --memsize 4096 \
     --size 30G \
@@ -11,13 +11,16 @@ virt-builder fedora-33 \
     --hostname gitlab-runner-fedora \
     --network \
     --run-command "rm -rf /etc/yum.repos.d/*modular*.repo /etc/yum.repos.d/fedora-cisco-openh264.repo; " \
-    --install git,git-lfs,openssh-server,curl,sudo,passwd,grub2-tools,devscripts,debootstrap,pbuilder,python3-sh,wget,createrepo,rpm,yum,yum-utils,mock,rsync,rpmdevtools,rpm-build,perl-Digest-MD5,perl-Digest-SHA,python3-pyyaml,hunspell,pandoc,jq,rubygems,ruby-devel,gcc-c++,pkg-config,libxml2,libxslt,libxml2-devel,libxslt-devel,rubygem-bundler,python3-pip \
-    --run-command "curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64" \
-    --run-command "chmod +x /usr/local/bin/gitlab-runner" \
+    --install curl,sudo,passwd,grub2-tools \
+    --run-command "curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.rpm.sh | bash" \
     --run-command 'useradd -m -p "" gitlab-runner -s /bin/bash' \
+    --install gitlab-runner,git,git-lfs,openssh-server \
     --run-command "git lfs install --skip-repo" \
     --ssh-inject gitlab-runner:file:/root/.ssh/id_rsa.pub \
     --run-command "rm -f /root/.ssh/know_hosts" \
+    --upload /home/user/ci-keys/id_rsa:/home/gitlab-runner/.ssh/id_rsa \
+    --run-command "chown gitlab-runner:gitlab-runner /home/gitlab-runner/.ssh/id_rsa" \
+    --run-command "chmod 0600 /home/gitlab-runner/.ssh/id_rsa" \
     --run-command "echo 'gitlab-runner ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers" \
     --run-command "sed -E 's/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"net.ifnames=0 biosdevname=0\"/' -i /etc/default/grub" \
     --run-command "grub2-mkconfig -o /boot/grub2/grub.cfg" \
@@ -25,4 +28,6 @@ virt-builder fedora-33 \
     --run-command "echo 'BOOTPROTO=dhcp' >> /etc/sysconfig/network-scripts/ifcfg-eth0" \
     --run-command "sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config" \
     --root-password password:root \
+    --install devscripts,debootstrap,pbuilder,git,python3-sh,wget,createrepo,rpm,yum,yum-utils,mock,rsync,rpmdevtools,rpm-build,perl-Digest-MD5,perl-Digest-SHA,python3-pyyaml \
+    --install hunspell,pandoc,jq,rubygems,ruby-devel,gcc-c++,pkg-config,libxml2,libxslt,libxml2-devel,libxslt-devel,rubygem-bundler \
     --update
