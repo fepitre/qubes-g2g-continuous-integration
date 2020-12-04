58007515d1ebb548ec8c646e2437af488287d36c
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2020-12-04 12:06:39 +0100

    Refactor GitCli
    Better handling of Gitlab/Github args

--- cli/git.py
+++ cli/git.py
@@ -2,42 +2,62 @@ import subprocess
 import re
 
 
+class GitException(Exception):
+    pass
+
+
 class GitCli:
     def __init__(self, repo):
         self.repo = repo
 
-    def clone(self, source, branch=None):
+    @staticmethod
+    def _run(cmd, cwd=None):
+        try:
+            result = subprocess.run(cmd, shell=True, cwd=cwd, check=True,
+                                    stderr=subprocess.STDOUT)
+            return result.stdout
+        except subprocess.CalledProcessError as e:
+            raise GitException(str(e))
+
+    @staticmethod
+    def _get_output(cmd, cwd=None):
+        try:
+            result = subprocess.check_output(cmd, shell=True, cwd=cwd,
+                                             stderr=subprocess.STDOUT)
+            return result.decode('utf-8').rstrip('\n')
+        except subprocess.CalledProcessError as e:
+            raise GitException(str(e))
+
+    def clone(self, source, branch=None, username='fepitre-bot',
+              useremail='fepitre-bot@qubes-os.org'):
         if branch:
             repo = 'git clone -b {branch} {src} {dst}'
         else:
             repo = 'git clone {src} {dst}'
-        result = subprocess.run(repo.format(
-            src=source, dst=self.repo, branch=branch), shell=True)
+        cmd = repo.format(src=source, dst=self.repo, branch=branch)
+        cmd_config_name = 'git config user.name "%s"' % username
+        cmd_config_email = 'git config user.email %s' % useremail
 
-        return result.returncode, result.stdout, result.stderr
+        self._run(cmd)
+        self._run(cmd_config_name, self.repo)
+        self._run(cmd_config_email, self.repo)
 
     def delete_remote_branch(self, source, branch):
-        cmd = 'git push {source} --delete {branch}'
-        result = subprocess.run(cmd.format(
-            source=source, branch=branch), shell=True, cwd=self.repo)
-
-        return result.returncode, result.stdout, result.stderr
+        cmd = 'git push {source} --delete {branch}'.format(
+            source=source, branch=branch)
+        self._run(cmd, self.repo)
 
     def reset(self, ref, hard=False):
         if hard:
-            cmd = 'git reset --hard {ref}'
+            cmd = 'git reset --hard {ref}'.format(ref=ref)
         else:
-            cmd = 'git reset {ref}'
-        result = subprocess.run(cmd.format(
-            ref=ref), shell=True, cwd=self.repo)
-
-        return result.returncode, result.stdout, result.stderr
+            cmd = 'git reset {ref}'.format(ref=ref)
+        self._run(cmd, self.repo)
 
     def fetch(self, source, branch='master'):
-        result = subprocess.run('git fetch {source} --tags {branch}'.format(
-            source=source, branch=branch), shell=True, cwd=self.repo)
-
-        return result.returncode, result.stdout, result.stderr
+        cmd = 'git fetch {source} --tags {branch}'.format(
+            source=source, branch=branch)
+        self._run(cmd, self.repo)
 
     def push(self, remote, branch='master', force=False):
         if force:
@@ -46,9 +66,7 @@ class GitCli:
         else:
             cmd = 'git push -u {remote} {branch}'.format(
                 remote=remote, branch=branch)
-        result = subprocess.run(cmd, shell=True, cwd=self.repo)
-
-        return result.returncode, result.stdout, result.stderr
+        self._run(cmd, self.repo)
 
     def checkout(self, ref, branch=None):
         if branch:
@@ -57,28 +75,31 @@ class GitCli:
         else:
             cmd = 'git checkout {ref}'.format(
                 ref=ref)
-        result = subprocess.run(cmd, shell=True, cwd=self.repo)
-
-        return result.returncode, result.stdout, result.stderr
+        self._run(cmd, self.repo)
 
     def remote_add(self, remote_name, url):
-        result = subprocess.run('git remote add {remote_name} {url}'.format(
-            remote_name=remote_name, url=url), shell=True, cwd=self.repo)
-
-        return result.returncode, result.stdout, result.stderr
+        cmd = 'git remote add {remote_name} {url}'.format(
+            remote_name=remote_name, url=url)
+        self._run(cmd, self.repo)
 
     def tags(self, ref):
-        tags = subprocess.check_output(
-            'git tag --points-at="{ref}"'.format(ref=ref),
-            shell=True, cwd=self.repo).decode('utf-8').rstrip('\n').split('\n')
-
+        cmd = 'git tag --points-at="{ref}"'.format(ref=ref)
+        result = self._get_output(cmd, self.repo)
+        tags = result.split('\n')
         return tags
 
     def verify_tag(self, tag, keyrings):
         cmd = 'GNUPGHOME={keyrings} git -c gpg.program=gpg verify-tag --raw {tag}'.format(
             keyrings=keyrings, tag=tag)
-
-        result = subprocess.check_output(
-            cmd, shell=True, cwd=self.repo, stderr=subprocess.STDOUT).decode('utf-8')
-
+        result = self._get_output(cmd, self.repo)
         return re.search(r'\[GNUPG:\] TRUST_(FULLY|ULTIMATE)', result)
+
+    def rev_parse(self, reference):
+        cmd = 'git rev-parse --short {reference}'.format(reference=reference)
+        result = self._get_output(cmd, self.repo)
+        return result
+
+    def merge(self, reference, message):
+        cmd = 'git merge {reference} --no-ff -m "{message}"'.format(
+                reference=reference, message=message)
+        self._run(cmd, self.repo)
--- create-gitlab-branch.py
+++ create-gitlab-branch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from cli.git import GitCli
+from cli.git import GitCli, GitException
 from cli.gitlab import GitlabCli
 
 import os
@@ -12,16 +12,23 @@ import shutil
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--clone', action='store', type=str, required=True,
-                    help='Git source repository')
-parser.add_argument('--push', action='store', type=str, required=True,
+parser.add_argument('--gitlab-url', action='store', type=str, required=True,
                     help='Gitlab instance url')
 parser.add_argument('--ref', action='store', type=str,
                     help='Git reference to use')
 parser.add_argument('--pull-request', action='store', type=int,
                     help='Git pullrequest reference to use')
-parser.add_argument('--repo', action='store', type=str, required=True,
-                    help='owner/project in Gitlab instance')
+parser.add_argument('--github-component', action='store', type=str,
+                    required=True,
+                    help='Github project name (e.g. qubes-linux-kernel)')
+parser.add_argument('--github-owner', action='store', type=str, required=True,
+                    help='Github owner of the project where the pullrequest '
+                         'is made.')
+parser.add_argument('--gitlab-component', action='store', type=str,
+                    required=True,
+                    help='Gitlab project name (e.g. qubes-linux-kernel)')
+parser.add_argument('--gitlab-owner', action='store', type=str, required=True,
+                    help='Gitlab owner of the project where the pipeline is ran')
 parser.add_argument('--verbose', action='store_true')
 parser.add_argument('--debug', action='store_true')
 
@@ -45,6 +52,10 @@ def main(args=None):
     if args.pull_request is not None and args.ref is not None:
         parser.error('Only one of --ref or --pull-request can be used')
 
+    if not os.environ.get('GITHUB_API_TOKEN', None):
+        logger.error("Cannot find GITHUB_API_TOKEN")
+        return 1
+
     if not os.environ.get('GITLAB_API_TOKEN', None):
         logger.error("Cannot find GITLAB_API_TOKEN")
         return 1
@@ -52,58 +63,58 @@ def main(args=None):
     tmpdir = tempfile.mkdtemp()
     try:
         git = GitCli(tmpdir)
-        logger.debug('Clone %s' % args.clone)
-        git.clone(args.clone)
+        github_url = 'https://github.com/{repo_owner}/{repo_name}'.format(
+            repo_owner=args.github_owner,
+            repo_name=args.github_component)
+        logger.debug('Clone %s' % github_url)
+        git.clone(github_url)
 
-        parsed_repo = args.repo.rstrip('/').split('/')
-        repo_owner = parsed_repo[0]
-        repo_name = parsed_repo[1]
+        repo_owner = args.gitlab_owner
+        repo_name = args.gitlab_component
 
         branch = args.ref
         if args.pull_request:
             branch = 'pr-%s' % args.pull_request
 
-        url = 'https://{repo_owner}:{token}@{gitlab_url}/{repo}'.format(
+        url = 'https://{repo_owner}:{token}@{gitlab_url}/{repo_owner}/{repo_name}'.format(
             token=os.environ['GITLAB_API_TOKEN'],
-            gitlab_url=args.push.replace('https://', ''), repo=args.repo,
+            gitlab_url=args.gitlab_url.replace('https://', ''),
+            repo_name=repo_name,
             repo_owner=repo_owner)
 
         logger.debug('Add remote %s' % repo_owner)
         git.remote_add(repo_owner, url)
 
         logger.debug('Delete remote branch %s' % branch)
-        git.delete_remote_branch(repo_owner, branch)
+        try:
+            git.delete_remote_branch(repo_owner, branch)
+        except GitException:
+            pass
 
-        fetchref_found = False
         if args.pull_request:
             remote_ref = '+refs/pull/%d/merge' % args.pull_request
             ref = 'FETCH_HEAD'
 
             logger.debug('Fetch {} {}'.format('origin', remote_ref))
-            exit_code_fetch, stdout, stderr = git.fetch('origin', remote_ref)
-            if exit_code_fetch == 0:
-                fetchref_found = True
-                logger.debug('Checkout %s' % ref)
-                git.checkout(ref, branch=branch)
-                git.reset(ref, hard=True)
+            git.fetch('origin', remote_ref)
+            logger.debug('Checkout %s' % ref)
+            git.checkout(ref, branch=branch)
+            git.reset(ref, hard=True)
         else:
             logger.debug('Fetch {} {}'.format('origin', args.ref))
-            exit_code_fetch, stdout, stderr = git.fetch('origin', args.ref)
-            if exit_code_fetch == 0:
-                fetchref_found = True
-                if args.ref != 'master':
-                    logger.debug('Checkout %s' % args.ref)
-                    git.checkout(args.ref, branch=branch)
+            git.fetch('origin', args.ref)
+            if args.ref != 'master':
+                logger.debug('Checkout %s' % args.ref)
+                git.checkout(args.ref, branch=branch)
 
-        if fetchref_found:
-            # Before pushing new branch we cancel previous running pipelines
-            # with same pr branch name
-            gitlabcli = GitlabCli(url='https://gitlab.com',
-                                  token=os.environ['GITLAB_API_TOKEN'])
-            gitlabcli.cancel_pipelines(repo_owner, repo_name, branch)
+        # Before pushing new branch we cancel previous running pipelines
+        # with same pr branch name
+        gitlabcli = GitlabCli(url='https://gitlab.com',
+                              token=os.environ['GITLAB_API_TOKEN'])
+        gitlabcli.cancel_pipelines(repo_owner, repo_name, branch)
 
-            logger.debug('Push to %s', repo_owner)
-            git.push(repo_owner, branch, force=True)
+        logger.debug('Push to %s', repo_owner)
+        git.push(repo_owner, branch, force=True)
     finally:
         shutil.rmtree(tmpdir)
 
