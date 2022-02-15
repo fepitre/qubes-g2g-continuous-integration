191dc29fa2903868ecabc9eafa5f155da35350bc
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2022-02-15 15:59:09 +0100

    runner: add script for automatic shutdown

---
+++ runner/g2g-idle-shutdown.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+rm -f /tmp/.started && echo > /tmp/.started
+
+# just in case...
+sleep 1
+
+# -2 == remove header/footer of journalctl
+while [ "$(( $(find /tmp/.started -mmin -180 | wc -l) + $(journalctl -t gitlab-runner --no-hostname -r --grep 'Submitting job to coordinator' --since '3 hours ago' | wc -l) - 2 ))" != "0" ]; do
+        sleep 15m
+done
+
+date >> /var/log/shutdown.log
+/usr/sbin/shutdown
