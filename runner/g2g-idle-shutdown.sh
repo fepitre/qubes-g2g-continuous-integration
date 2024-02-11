82488992394dbc42b149dc0b10ad92629c1292fd
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2024-02-11 15:08:51 +0100

    Update shutdown script

--- runner/g2g-idle-shutdown.sh
+++ runner/g2g-idle-shutdown.sh
@@ -8,8 +8,8 @@ rm -f /tmp/.started && echo > /tmp/.started
 sleep 1
 
 # -2 == remove header/footer of journalctl
-while [ "$(( $(find /tmp/.started -mmin -180 | wc -l) + $(journalctl -t gitlab-runner --no-hostname -r --grep 'Submitting job to coordinator' --since '3 hours ago' | wc -l) - 2 ))" != "0" ]; do
-        sleep 15m
+while [ "$(( $(find /tmp/.started -mmin -180 | wc -l) + $(journalctl -t gitlab-runner --no-hostname -r --grep 'Submitting job to coordinator' --since '3 hours ago' | wc -l) - 2 + $(find /tmp/.started /var/lib/openqa/pool/ -mmin -180 | wc -l) ))" != "0" ]; do
+    sleep 15m
 done
 
 date >> /var/log/shutdown.log
