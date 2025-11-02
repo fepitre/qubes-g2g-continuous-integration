a24aff1c84676cdf4b94e59f81784f0e015b4323
Frédéric Pierret (fepitre) <frederic@invisiblethingslab.com>
2025-11-02 12:14:53 +0100

    dom0-net: drop iptables usage

--- runner/kvm/setup-dom0-net.sh
+++ runner/kvm/setup-dom0-net.sh
@@ -17,6 +17,7 @@ rm -f /etc/resolv.conf
 echo -e 'nameserver 10.139.1.1\nnameserver 10.139.1.2' > /etc/resolv.conf
 qvm-run -p --no-gui -u root sys-net systemctl stop qubes-firewall
 sleep 2
-qvm-run -p --no-gui -u root sys-net iptables -t nat -I PREROUTING '!' -i vif+ -p tcp --dport 22 -j DNAT --to 10.137.99.1
-qvm-run -p --no-gui -u root sys-net iptables -I FORWARD -p tcp --dport 22 -d 10.137.99.1 -j ACCEPT || :
-qvm-run -p --no-gui -u root sys-net nft add rule ip qubes custom-forward tcp dport ssh accept || :
+
+qvm-run -p --no-gui -u root sys-net nft 'add chain ip qubes prerouting_nat { type nat hook prerouting priority 100; }'
+qvm-run -p --no-gui -u root sys-net nft add rule ip qubes prerouting_nat iifname != "vif*" tcp dport 22 dnat to 10.137.99.1
+qvm-run -p --no-gui -u root sys-net nft add rule ip qubes custom-forward tcp dport ssh accept
